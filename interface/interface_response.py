443232b09f8a6fe48e6a8c99b5bd928334dceaa3
June <68359573+CT1138@users.noreply.github.com>
2025-06-24 22:09:50 -0700

    Update interface_response.py

--- interface/interface_response.py
+++ interface/interface_response.py
@@ -13,25 +13,25 @@ class IF_Response:
     def __init__(self):
         self.db = IF_Database()
 
-    async def getResult(self, key: str, param: str, result_type: ResultType):
+    async def getResult(self, key: str, result_type: ResultType):
         await self.db.connect()
 
         if result_type == ResultType.RESPONSE:
-            data = self.db.fetch(SQLCommands.GET_RESPONSES.value, (key,), all=True)
+            data = self.db.fetch(SQLCommands.GET_RESPONSES.value, (key), all=True)
             values = [row["content"] for row in data] if data else []
             return uString.shorten_string(values, 2000)
 
         elif result_type == ResultType.URL:
-            data = self.db.fetch(SQLCommands.GET_GIFS.value, (key,), all=True)
+            data = self.db.fetch(SQLCommands.GET_GIFS.value, (key), all=True)
             values = [row["content"] for row in data] if data else []
             return uString.shorten_string(values, 2000)
 
         elif result_type == ResultType.MEMORY:
-            data = self.db.fetch(SQLCommands.GET_MEMORY.value, (key, param), all=True)
+            data = self.db.fetch(SQLCommands.GET_MEMORY.value, (key), all=True)
             return [row["content"] for row in data] if data else []
 
         elif result_type == ResultType.QUOTEBOOK:
-            data = self.db.fetch(SQLCommands.GET_QUOTEBOOK.value, (key,), all=True)
+            data = self.db.fetch(SQLCommands.GET_QUOTEBOOK.value, (key), all=True)
             return [row["content"] for row in data] if data else []
 
         else:
