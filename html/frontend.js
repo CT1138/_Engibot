4fc7e94a18df5b46a3160d6526accd8a11e2f2ee
June <68359573+CT1138@users.noreply.github.com>
2025-06-25 18:33:40 -0700

    Refactoring restyling

--- html/frontend.js
+++ html/frontend.js
@@ -2,7 +2,7 @@ async function loadGuilds() {
   try {
     const res = await fetch("http://127.0.0.1:8000/guilds");
     if (!res.ok) throw new Error("Network response was not ok");
-
+    console.log("Fetching guilds...");
     const guilds = await res.json();
 
     document.getElementById("guilds").textContent = guilds.length;
