4b5e07dfa1c0c9ed407c63f6e0c4c80bf249bad4
June <68359573+CT1138@users.noreply.github.com>
2025-07-24 18:49:26 -0700

    Update interface_ntfy.py

--- interface/interface_ntfy.py
+++ interface/interface_ntfy.py
@@ -1,13 +1,13 @@
 import requests, os
 
-# host|topic
-URL = os.getenv("NTFY")
-HOST, TOPIC = URL.split('|', 1)
 
 class IF_NTFY:
     @staticmethod
     def post(msg: str):
+        # host|topic
+        URL = os.getenv("NTFY")
         if not URL: return
+        HOST, TOPIC = URL.split('|', 1)
         url = f"http://{HOST}/{TOPIC}"
         print(f"[NTFY] Posted \"{msg}\" to listeners")
         requests.post(url, data=msg)
